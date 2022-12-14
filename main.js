15442299374d624c441dcbc21a6811202e33211e
TwoMal2 <x__x73@naver.com>
2022-12-14 18:52:35 +0900

    [add] express로 html 렌더링

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-const port = 80,
+const port = 3000,
     express = require("express"),
-    app = express();
+    app = express(),
+    errorController = require("./controllers/errorController");
 
 
+app.set("port", process.env.PORT || 80);
+app.use("/public", express.static("public"));
+
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname + "/views/index.html");
+});
     
+
 
 app.listen(app.get("port"), () => {
     console.log(`Server running at http://localhost:${app.get("port")}`);
-  });
+});
   
